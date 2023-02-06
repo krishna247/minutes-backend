be3c07e7b169c03813c49d53ae8f26f51026fa5c
Krishna <iamkrishna.krishna@gmail.com>
2023-02-06 23:11:24 +0530

    Added application.properties without creds

--- src/main/java/app/tasks/handler/S3Handler.java
+++ src/main/java/app/tasks/handler/S3Handler.java
@@ -6,6 +6,7 @@ import app.tasks.utils.AuthUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class S3Handler {
     S3Upload s3Upload;
     @Autowired
     AuthUtils authUtils;
+    @Autowired Environment env;
 
     @Operation(security = {@SecurityRequirement(name = "Authorization")})
     @GetMapping("/upload")
@@ -29,4 +31,18 @@ public class S3Handler {
         authUtils.isAuthenticated(sessionToken, sessionRepository);
         return Map.of("url", s3Upload.getPresignedUrl(key, contentType));
     }
+
+//    @Operation(security = {@SecurityRequirement(name = "Authorization")})
+//    @GetMapping("/listS3")
+//    public void listS3files(@RequestParam String prefix){
+//        S3Client client = S3Client.builder().region(Region.EU_WEST_1).build();
+//        ListObjectsV2Request request = ListObjectsV2Request.builder().bucket(env.getProperty("aws.s3.resource_bucket")).prefix(prefix).build();
+//        ListObjectsV2Iterable response = client.listObjectsV2Paginator(request);
+//
+//        for (ListObjectsV2Response page : response) {
+//            page.contents().forEach((S3Object object) -> {
+//                System.out.println(object.key());
+//            });
+//        }
+//    }
 }
